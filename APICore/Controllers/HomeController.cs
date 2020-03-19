ee29848a39c635ffac7b4b8a569343ef80280c7d
BlackFace <bf.blackface@gmail.com>
2020-03-19 23:03:06 +0700

    Add project files.

---
+++ APICore/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using APICore.CoreBase;
+using Microsoft.AspNetCore.Mvc;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APICore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string id)
+        {
+            var a = Path.GetFileNameWithoutExtension("DevExpress.XtraGauges.v19.1.Presets.xml");
+            return Ok("BlackFace API");
+        }
+    }
+}
---
+++ APICore/CoreBase/Security.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace APICore.CoreBase
+{
+    public static class Security
+    {
+        /// <summary>
+        /// Tạo mã MD5 cho file
+        public static string GenMD5File(string filePath)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = System.IO.File.OpenRead(filePath))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+    }
+}
